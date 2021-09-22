be7ac4384f9ce5d47561ef6c45e836c120e147ed
Geraldo-git <laboratorio.inicial@gmail.com>
2021-09-22 10:33:42 -0300

    Pagination

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -3,3 +3,31 @@ INSERT INTO tb_category (name, created_at) VALUES ('Electronics', Now());
 INSERT INTO tb_category (name, created_at) VALUES ('Computers', Now());
 INSERT INTO tb_category (name, created_at) VALUES ('Garden', Now());
 INSERT INTO tb_category (name, created_at) VALUES ('Pets', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Books', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Electronics', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Computers', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Garden', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Pets', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Books', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Electronics', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Computers', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Garden', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Pets', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Books', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Electronics', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Computers', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Garden', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Pets', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Books', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Electronics', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Computers', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Garden', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Pets', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Books', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Electronics', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Computers', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Garden', Now());
+INSERT INTO tb_category (name, created_at) VALUES ('Pets', Now());
+
+
+
