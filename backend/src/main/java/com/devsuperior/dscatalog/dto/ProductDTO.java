bed65dd2fdd4744e0c0b5149ce30a5c692c99161
Geraldo-git <laboratorio.inicial@gmail.com>
2021-10-06 09:15:08 -0300

    Initial constraints

--- backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -6,18 +6,27 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
 
-public class ProductDTO implements Serializable{
+public class ProductDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	
+
 	private Long id;
+	@Size(min = 5,max =60, message="Deve ter entre 5 e 60 caracteres")
+	@NotBlank(message = "Campo requerido")
 	private String name;
+	@NotBlank(message = "Campo requerido")
 	private String description;
+	@Positive(message = "Preço deve ser um valor positivo")
 	private Double price;
 	private String imgUrl;
+	@PastOrPresent(message = "Data de cadastro não pode ser futura")
 	private Instant date;
 
 	private List<CategoryDTO> categories = new ArrayList<>();
@@ -43,7 +52,7 @@ public class ProductDTO implements Serializable{
 		imgUrl = entity.getImgUrl();
 		date = entity.getDate();
 	}
-	
+
 	public ProductDTO(Product entity, Set<Category> categories) {
 		this(entity);
 		categories.forEach(cat -> this.categories.add(new CategoryDTO(cat)));
@@ -104,9 +113,5 @@ public class ProductDTO implements Serializable{
 	public void setCategories(List<CategoryDTO> categories) {
 		this.categories = categories;
 	}
-	
-	
-	
-	
 
 }
